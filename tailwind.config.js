4e500723d8783fda2d193e3d833bc7a246c5d1ae
suraj mandal <rjsurajmandal@gmail.com>
2023-02-12 13:04:20 +0530

    update

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily : {
+        Rubik : ["Rubik","sans-serif"],
+      },
+      colors: {
+        lightBlue: "#6674cc",
+      },
+    },
+  },
+  plugins: [],
+}
